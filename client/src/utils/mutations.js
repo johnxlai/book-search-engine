7743c25160980485423516f72fae6e83dd57dc95
John Lai <lai.john.py@gmail.com>
2023-03-27 22:55:06 -0400

    Update mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -42,3 +42,11 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      bookId
+    }
+  }
+`;
